3bbecbff6d40311376201ced070cafdb1232df26
Waqas Raza <waqasraza123@gmail.com>
2024-10-20 16:02:52 +0500

    added infinite scroll on home data screen

--- lib/src/data_screen.dart
+++ lib/src/data_screen.dart
@@ -12,6 +12,10 @@ class DataScreen extends StatefulWidget {
 class DataScreenState extends State<DataScreen> {
   final DexApiService apiService = DexApiService();
   late Future<List<dynamic>> dexData;
+  List<dynamic> tokens = [];
+  int itemsToLoad = 10; // Initial number of tokens to load
+  int currentItemCount = 0; // Keeps track of how many tokens are loaded
+  bool isLoadingMore = false;
 
   @override
   void initState() {
@@ -33,17 +37,60 @@ class DataScreenState extends State<DataScreen> {
             return const Center(child: Text('No Data Found'));
           }
 
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              var token = snapshot.data![index];
-              return TokenCard(
-                token: token,
-              );
+          // Load initial set of tokens
+          if (tokens.isEmpty) {
+            tokens = snapshot.data!.take(itemsToLoad).toList();
+            currentItemCount = tokens.length;
+          }
+
+          return NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification scrollInfo) {
+              if (!isLoadingMore &&
+                  scrollInfo.metrics.pixels ==
+                      scrollInfo.metrics.maxScrollExtent) {
+                _loadMoreTokens(snapshot.data!);
+              }
+              return false;
             },
+            child: ListView.builder(
+              itemCount: tokens.length + (isLoadingMore ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index == tokens.length) {
+                  return const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+
+                var token = tokens[index];
+                return TokenCard(token: token);
+              },
+            ),
           );
         },
       ),
     );
+  }
+
+  // Load more tokens when the user scrolls to the bottom
+  void _loadMoreTokens(List<dynamic> allTokens) async {
+    if (currentItemCount >= allTokens.length) return;
+
+    setState(() {
+      isLoadingMore = true;
+    });
+
+    await Future.delayed(
+        const Duration(seconds: 2)); // Simulate delay for loading
+
+    setState(() {
+      int nextItemCount = currentItemCount + itemsToLoad;
+      if (nextItemCount > allTokens.length) {
+        nextItemCount = allTokens.length;
+      }
+      tokens.addAll(allTokens.getRange(currentItemCount, nextItemCount));
+      currentItemCount = tokens.length;
+      isLoadingMore = false;
+    });
   }
 }
