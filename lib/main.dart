8963c8d8c97de84bd41c3ac713144f394d59ab59
Waqas Raza <waqasraza123@gmail.com>
2024-10-22 19:14:29 +0500

    Resolved: A platform implementation for webview_flutter has not been set. Please ensure that an implementation of WebViewPlatform has been set to WebViewPlatform.instance before use

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,26 @@ import './src/auth/register_screen.dart';
 import './src/main/main_screen.dart';
 import './src/main/theme.dart';
 import 'src/auth/magic_link/email_otp_login_screen.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:webview_flutter_web/webview_flutter_web.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: "assets/config/.env");
+
+  // Set the platform-specific WebView implementation for Android and iOS
+  if (WebViewPlatform.instance == null) {
+    if (kIsWeb) {
+      WebViewPlatform.instance =
+          WebWebViewPlatform(); // Set Web platform implementation
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
+      WebViewPlatform.instance = AndroidWebViewPlatform();
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+      WebViewPlatform.instance = WebKitWebViewPlatform();
+    }
+  }
 
   // Initialize Magic Link
   Magic.instance = Magic(dotenv.env['MAGIC_LINK_PUBLISHABLE_KEY']!);
